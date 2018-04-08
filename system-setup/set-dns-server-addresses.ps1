783d7c577a57665f59bc354246894741bbcbd3df
Alan Płócieniak <alan-null@outlook.com>
2018-04-08 08:43:32 +0200

    set-dns-server-addresses.ps1
    Added Cloudflare address
    Added IPv6 address for Google DNS

--- system-setup/set-dns-server-addresses.ps1
+++ system-setup/set-dns-server-addresses.ps1
@@ -1,4 +1,12 @@
-$DNS = ("8.8.8.8", "8.8.4.4")
+Write-Host '1: Cloudflare'
+Write-Host '2: Google'
+$dnsProviderIndex = Read-Host 'DNS provider: '
+if ($dnsProviderIndex -eq 2) {
+    $DNS = ("8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844")
+}
+else {
+    $DNS = ("1.1.1.1", "1.0.0.1", "2606:4700:4700::1111", "2606:4700:4700::1001")
+}
 
 $interfaces = Get-NetIPInterface
 $interfaces
