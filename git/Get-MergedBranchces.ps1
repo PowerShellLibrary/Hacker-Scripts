1d221131595ff6667babdb86c3faf61415ee8ea8
Alan Płócieniak <alan-null@outlook.com>
2019-11-23 15:08:12 +0100

    Added Get-MergedBranchces.ps1

---
+++ git/Get-MergedBranchces.ps1
@@ -0,0 +1,36 @@
+<#
+
+.SYNOPSIS
+Helper script for listing merged branches
+
+.DESCRIPTION
+This script will return all branches names that were already merged to the current branch
+
+.PARAMETER Branches
+Collection of excluded branches
+
+.EXAMPLE
+.\Get-MergedBranchces.ps1
+Gets all merged branches except default branches: master, develop
+
+.EXAMPLE
+.\Get-MergedBranchces.ps1 -Branches naster,feature1
+Gets all merged branches except specified branches: master, feature1
+
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $false)]
+    [string[]]$Branches = @("master", "develop")
+)
+
+process {
+    $testIsInWorkFree = git rev-parse --is-inside-work-tree
+    if ($testIsInWorkFree) {
+        git branch --merged | % { $_.TrimStart([string]::Empty) } | ? { $Branches.Contains($_) -eq $false } | ? { -not($_.StartsWith("*")) }
+    }
+    else {
+        Write-Host "This is not a git repository."
+    }
+}
