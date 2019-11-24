68197d427bfe8121e71ff43df329d43f2f454ba0
Alan Płócieniak <alan-null@outlook.com>
2019-11-24 02:53:56 +0100

    Added Remove-MergedBranches.ps1

---
+++ git/Remove-MergedBranches.ps1
@@ -0,0 +1,32 @@
+<#
+
+.SYNOPSIS
+Helper script for removing merged branches
+
+.DESCRIPTION
+This script will remove all branches which were already merged to the current branch
+
+.PARAMETER Branches
+Collection of excluded from removing branches
+
+.EXAMPLE
+.\Remove-MergedBranches.ps1
+Removes all merged branches except default branches: master, develop
+
+.EXAMPLE
+.\Remove-MergedBranches.ps1 -Branches naster,feature1
+Removes all merged branches except specified branches: master, feature1
+
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $false)]
+    [string[]]$Branches = @("master", "develop")
+)
+
+process {
+    . $PSScriptRoot\Get-MergedBranchces.ps1 $Branches | % {
+        git branch -d $_
+    }
+}
