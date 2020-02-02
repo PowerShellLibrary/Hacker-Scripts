f3ae493e860abde9462c60b8ef92ca57a723ebe0
Alan Płócieniak <alan-null@outlook.com>
2020-02-02 14:54:56 +0100

    Added create-file-snapshot.ps1

---
+++ system-maintenance/create-file-snapshot.ps1
@@ -0,0 +1,25 @@
+param(
+    [parameter(Mandatory = $true, Position = 0)]
+    [string]$fileToBackup,
+    [parameter(Mandatory = $true, Position = 1)]
+    [string]$backupFolderPath
+)
+Import-Module NullKit
+
+$current = Get-Location
+try {
+    Set-Location $backupFolderPath
+    if (!(Test-GitRepo)) {
+        git init
+    }
+
+    if (Test-GitRepo) {
+        Copy-Item -Path $fileToBackup -Destination $backupFolderPath -Force
+        git add .
+        $date = Get-Date
+        git commit -m "$date"
+    }
+}
+finally {
+    Set-Location $current
+}
