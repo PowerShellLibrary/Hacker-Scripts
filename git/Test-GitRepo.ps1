b4d3c63f42a512e53780e251bfdfed02d8f8a319
Alan Płócieniak <alan-null@outlook.com>
2020-02-02 14:54:17 +0100

    Added Test-GitRepo.ps1

---
+++ git/Test-GitRepo.ps1
@@ -0,0 +1,24 @@
+function Test-GitRepo {
+    <#
+.SYNOPSIS
+Helper script for listing merged branches
+
+.DESCRIPTION
+This script will return all branches names that were already merged to the current branch
+
+.PARAMETER Branches
+Collection of excluded branches
+
+.EXAMPLE
+.\Test-GitRepo.ps1
+Gets all merged branches except default branches: master, develop
+#>
+
+    [CmdletBinding()]
+    param()
+
+    process {
+        $testIsInWorkFree = git rev-parse --is-inside-work-tree
+        $testIsInWorkFree -eq "true"
+    }
+}
