9bdc28ec61dff7b817b52ed71cafd0b2633b9b99
Alan Płócieniak <alan-null@outlook.com>
2020-04-12 17:47:51 +0200

    Added update-net-framework-version.ps1

---
+++ development/.net/update-net-framework-version.ps1
@@ -0,0 +1,16 @@
+# configuration
+$targetFramework = "net48"
+$startPath = "C:\repo\Sitecore\ContentTagging\code"
+# configuration - END
+
+Clear-Host
+Get-ChildItem -Path $startPath -Recurse -ErrorAction SilentlyContinue | ? { $_.Extension -eq ".csproj" } | % {
+    Write-Host "Processing $_" -ForegroundColor Green
+    [xml]$xmlDoc = Get-Content $_.FullName
+    [System.Xml.XmlNode]$node = $xmlDoc.Project.PropertyGroup.ChildNodes | ? { $_.Name -eq "TargetFramework" }
+    if ($node.InnerText -ne $targetFramework) {
+        Write-Host "`tChanking value from '$($node.InnerText)' to '$targetFramework'" -ForegroundColor DarkCyan
+        $node.InnerText = $targetFramework
+        $xmlDoc.Save($_.FullName)
+    }
+}
