82d98e9fcb339fa5a562fa21c907550e85f8d07a
Alan Płócieniak <alan-null@outlook.com>
2020-04-14 19:23:47 +0200

    NullKit cleanup.

--- Run-PSScriptAnalyzer.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module "$env:USERPROFILE\.vscode\extensions\ms-vscode.powershell-*\modules\PSScriptAnalyzer\*\PSScriptAnalyzer.psm1"
-
-$Public = Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "*.ps1" | ? { $_.Name[0].Equals($_.Name.ToUpper()[0]) }
-
-$Public | % { Invoke-ScriptAnalyzer -Path $_.fullname -Settings .\PSScriptAnalyzerSettings.psd1 }
