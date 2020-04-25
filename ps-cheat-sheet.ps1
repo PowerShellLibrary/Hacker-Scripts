46876fdd80e3bfcda8374b4db288876e9ebc6d3a
Alan Płócieniak <alan-null@outlook.com>
2020-04-25 22:36:42 +0200

    PowerShell cheat sheet
    discover PS

---
+++ ps-cheat-sheet.ps1
@@ -0,0 +1,24 @@
+Clear-Host
+
+Write-Host "Get-Alias dir, cls" -ForegroundColor Green
+Get-Alias dir, cls
+
+Write-Host "Get-Command -Name get-* -Module NullKit" -ForegroundColor Green
+Get-Command -Name get-* -Module NullKit
+
+Write-Host "Get-Command -Module NullKit" -ForegroundColor Green
+Get-Command -Module NullKit
+
+Write-Host "Get-Command | Group-Object Verb | Sort-Object Count -Descending" -ForegroundColor Green
+Get-Command | Group-Object Verb | Sort-Object Count -Descending
+
+Write-Host "Get-Command | Group-Object Noun | Sort-Object Name" -ForegroundColor Green
+Get-Command | Group-Object Noun | Sort-Object Name | Sort-Object Count -Descending | Select-Object -First 10
+
+Write-Host "[System.DateTime] | Get-Member -MemberType Method Get*" -ForegroundColor Green
+[System.DateTime] | Get-Member -MemberType Method Get*
+
+Write-Host Get-ItemProperty -ForegroundColor Green
+Get-Item "C:\Windows\System32\drivers\etc\hosts" | Get-ItemProperty | Format-List -Property *
+
+Get-Module -ListAvailable
