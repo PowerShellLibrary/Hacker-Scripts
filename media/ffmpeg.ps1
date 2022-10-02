7b111b770cadf0b5ae06154b09512e92f5e828bb
Alan Płócieniak <alan-null@outlook.com>
2022-10-02 22:14:41 +0200

    Added ffmpeg for audio

--- media/ffmpeg.ps1
+++ media/ffmpeg.ps1
@@ -1,5 +1,5 @@
 # https://ffmpeg.org/ffmpeg.html
-# Take two files (audio, video) and combine them together into mp4 file.
+# Takes two files (audio, video) and combine them together into mp4 file.
 # As audio is shorter add offset to the video so it can sync with audio
 #
 # -y (global)       - overwrite output files without asking.
@@ -10,3 +10,13 @@
 # -map 1:0          - map stream 1 to output from input2
 # -c                - for -c[:stream_specifier] codec. copy (output only) to indicate that the stream is not to be re-encoded
 ffmpeg -y -itsoffset 0.2 -i v.webm -i a.mp4 -map 0:0 -map 1:0 -c copy oFilename.mp4
+
+
+# Converts wav to mp3
+#
+# -i path             input - mp3
+# -vn                 disable video
+# -ar rate            set audio sampling rate (in Hz)
+# -ac channels        set number of audio channels
+# -ab bitrate         audio bitrate (please use -b:a)
+ffmpeg -i "$($_.FullName)" -vn -ar 44100 -ac 2 -b:a 192k "$($_.FullName.Replace('.wav','.mp3'))"
