4904e0a41f210ab0757410aefab5897101863280
jantari <jantari@github.com>
2022-10-09 16:08:16 +0200

    add good-practice example of not using += with arrays

---
+++ good-practices/add-to-array.ps1
@@ -0,0 +1,17 @@
+﻿# Adding to an array with += is much slower:
+
+$list = @()
+Measure-Command {
+    foreach ($number in $(1..10000)) {
+        $list += $number
+    }
+}
+
+# than capturing the output of the loop directly:
+
+Measure-Command {
+    $list = foreach ($number in $(1..10000)) {
+        $number
+    }
+}
+
