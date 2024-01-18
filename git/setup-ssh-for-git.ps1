0488524e4db7fc95ffcd32b9e00d1658beab10bc
Alan Płócieniak <alan-null@outlook.com>
2024-01-18 19:32:04 +0100

    Added setup-ssh-for-git.ps1

---
+++ git/setup-ssh-for-git.ps1
@@ -0,0 +1,30 @@
+# Prerequisites - you need to get SSH key supported by your git hosting service.
+# deploy it into: c:\id_rsa
+
+# install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# install dependencies
+choco install git -y
+
+# deploy SSH key into keys store
+$userName = [Environment]::UserName
+Write-Host "Username: $userName" -ForegroundColor Green
+Get-Item c:\id_rsa | Copy-Item -Destination "C:\Users\$userName\.ssh\id_rsa"
+
+# enable ssh service
+Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+Start-Service ssh-agent
+
+# register ssh key
+ssh-add "C:\Users\$userName\.ssh\id_rsa"
+
+# register known_hosts
+ssh-keyscan bitbucket.org >> "C:\Users\$userName\.ssh\known_hosts"
+ssh-keyscan github.com >> "C:\Users\$userName\.ssh\known_hosts"
+ssh-keyscan vs-ssh.visualstudio.com >> "C:\Users\$userName\.ssh\known_hosts"
+
+# test connection to git repositories
+ssh -T ssh://alan-null@vs-ssh.visualstudio.com
+ssh -T git@bitbucket.org
+ssh -T git@github.com
