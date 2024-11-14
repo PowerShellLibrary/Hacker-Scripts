01dacc553784a3b9a3a3e8d8a2e296b01f5f4fa6
Alan Płócieniak <alan-null@outlook.com>
2024-11-14 22:12:21 +0100

    ui interaction with powershell

---
+++ windows/ui interaction with powershell.ps1
@@ -0,0 +1,13 @@
+﻿# Focus Assist Off
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("(^{ESC})")
+Start-Sleep -Milliseconds 500
+[System.Windows.Forms.SendKeys]::SendWait("(Focus Assist)")
+Start-Sleep -Milliseconds 200
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -Milliseconds 700
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB} ")
+Start-Sleep -Milliseconds 500
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -Milliseconds 500
+[System.Windows.Forms.SendKeys]::SendWait("(%{F4})")
