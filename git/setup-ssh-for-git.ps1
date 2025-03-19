2dd702fe5515207d833d81ba82cd2b13579258d1
Alan Płócieniak <alan-null@outlook.com>
2025-03-19 23:47:44 +0100

    generate SSH key snipept

--- git/setup-ssh-for-git.ps1
+++ git/setup-ssh-for-git.ps1
@@ -1,6 +1,10 @@
 # Prerequisites - you need to get SSH key supported by your git hosting service.
 # deploy it into: c:\id_rsa
 
+# generate SSH key
+ssh-keygen -t rsa -b 4096 -C "key-comment" # 4096 RSA key (SHA256)
+ssh-keygen -t ed25519 -C "key-comment"
+
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
