defcba3315e31bacbad1e6321a4dfef51417da16
Alan Płócieniak <alan-null@outlook.com>
2026-01-22 20:11:28 +0100

    Add plink script for SSH connection to GitHub
    Cache GitHub's SSH key fingerprints

---
+++ git/plink.sh
@@ -0,0 +1,2 @@
+# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
+& "C:\Program Files (x86)\Atlassian\Sourcetree*\tools\putty\plink.exe" github.com
